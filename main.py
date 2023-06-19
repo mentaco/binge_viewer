691f5cd70f374537424e1fd20e7afbb09b3678cc
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-06-19 10:37:39 +0900

    fix login function

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ import sys
 import getpass
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
 def login(driver):
-    userid = driver.find_element(By.ID, "login-username")
-    password = driver.find_element(By.ID, "login-password")
+    while True:
+        userid = driver.find_element(By.ID, "login-username")
+        password = driver.find_element(By.ID, "login-password")
 
-    _id = input("your id > ")
-    _passwd = getpass.getpass("your password > ")
-    userid.send_keys(_id)
-    password.send_keys(_passwd)
+        _id = input("your id > ")
+        _passwd = getpass.getpass("your password > ")
+        userid.send_keys(_id)
+        password.send_keys(_passwd)
 
-    login_button = driver.find_element(By.ID, "btn-login")
-    login_button.click()
+        login_button = driver.find_element(By.ID, "btn-login")
+        login_button.click()
+
+        sleep(1)
+
+        if "ログインに失敗" in driver.page_source:
+            print("Login failed.")
+            
+        elif "指定回数を超えた" in driver.page_source:
+            print("Authentication locked.")
+            return 1
+        else:
+            print("Login success.")
+            return 0
+
 
 def video_view(url):
     service = Service(executable_path=GeckoDriverManager().install())
     driver = webdriver.Firefox(service=service)
     driver.get(url)
 
-    login(driver)
+    if login(driver):
+        driver.quit()
 
-    sleep(3)
-    driver.quit()
+    # play_btn = driver.find_element(By.CLASS_NAME, "vjs-big-play-button")
+    # play_btn.click()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
