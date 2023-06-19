93ed8baa2393a92b70f5f4ad42db0fb8836a1c1d
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-06-19 11:13:17 +0900

    fix waiting process

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 import sys
 import getpass
-from time import sleep
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
-def login(driver):
+
+def login(driver, wait):
     while True:
-        userid = driver.find_element(By.ID, "login-username")
-        password = driver.find_element(By.ID, "login-password")
+        try:
+            userid = driver.find_element(By.ID, "login-username")
+            password = driver.find_element(By.ID, "login-password")
 
-        _id = input("your id > ")
-        _passwd = getpass.getpass("your password > ")
-        userid.send_keys(_id)
-        password.send_keys(_passwd)
+            _id = input("your id > ")
+            _passwd = getpass.getpass("your password > ")
+            userid.send_keys(_id)
+            password.send_keys(_passwd)
 
-        login_button = driver.find_element(By.ID, "btn-login")
-        login_button.click()
+            login_button = driver.find_element(By.ID, "btn-login")
+            login_button.click()
 
-        sleep(1)
+            wait.until(EC.presence_of_all_elements_located)
 
-        if "ログインに失敗" in driver.page_source:
-            print("Login failed.")
-            
-        elif "指定回数を超えた" in driver.page_source:
-            print("Authentication locked.")
+            if "ログインに失敗" in driver.page_source:
+                print("Login failed.")
+                
+            elif "指定回数を超えた" in driver.page_source:
+                print("Authentication locked.")
+                return 1
+            else:
+                print("Login success.")
+                return 0
+        except Exception as e:
+            print(e)
+            print("An error has occurred.")
             return 1
-        else:
-            print("Login success.")
-            return 0
 
 
 def video_view(url):
     service = Service(executable_path=GeckoDriverManager().install())
     driver = webdriver.Firefox(service=service)
+
+    wait = WebDriverWait(driver=driver, timeout=15)
+
     driver.get(url)
 
-    if login(driver):
+    if login(driver, wait):
         driver.quit()
 
     # play_btn = driver.find_element(By.CLASS_NAME, "vjs-big-play-button")
