27ccf6b501c97c7df6af7f895e59dfc2af93ede0
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-09-30 17:53:00 +0900

    create checker.py

---
+++ checker.py
@@ -0,0 +1,105 @@
+import sys
+import getpass
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+class BingeViewer:
+    def __init__(self, url):
+        self.start_url = url
+        self.service = Service(executable_path=GeckoDriverManager().install())
+        self.driver = webdriver.Firefox(service=self.service)
+        self.wait = WebDriverWait(driver=self.driver, timeout=15)
+    
+    def start_viewing(self):
+        self.driver.get(self.start_url)
+
+        if self.login():
+            self.driver.quit()
+
+        self.wait.until(EC.presence_of_all_elements_located)
+
+        while True:
+            sleep(5)
+            self.video_view()
+
+            if self.next_video():
+                break
+
+        self.driver.quit()
+
+    def login(self):
+        self.wait.until(EC.presence_of_all_elements_located)
+
+        while True:
+            try:
+                userid = self.driver.find_element(By.ID, "login-username")
+                password = self.driver.find_element(By.ID, "login-password")
+
+                userid.send_keys(input("your id > "))
+                password.send_keys(getpass.getpass("your password > "))
+
+                login_button = self.driver.find_element(By.ID, "btn-login")
+                login_button.click()
+
+                self.wait.until(EC.presence_of_all_elements_located)
+
+                if "ログインに失敗" in self.driver.page_source:
+                    print("Login failed.")
+                    
+                elif "指定回数を超えた" in self.driver.page_source:
+                    print("Authentication locked.")
+                    return 1
+                else:
+                    print("Login success.")
+                    return 0
+            except Exception as e:
+                print(e)
+                print("An error has occurred.")
+                return 1
+
+    def video_view(self):
+        self.video_title = self.driver.find_element(By.XPATH, "//section[@id='region-main']/div/h2")
+        # print(f"Start viewing \"{self.video_title.text}\"")
+        print(f"Check \"{self.video_title.text}\"")
+
+        # play_btn = self.driver.find_element(By.CLASS_NAME, "vjs-big-play-button")
+        # play_btn.click()
+    
+    def next_video(self):
+        # while True:
+        #     remaining_t = self.driver.find_element(By.CLASS_NAME, "vjs-remaining-time-display")
+        #     if remaining_t.text == "0:00":
+        #         break
+        #     sleep(15)
+        # print(f"Finish viewing \"{self.video_title.text}\"")
+
+        # completion_btn = self.driver.find_element(By.XPATH, "//div[@data-region='completion-info']/button")
+        # completion_btn.click()
+        # sleep(1)
+
+        comp_txt = self.driver.find_element(By.CLASS_NAME, "complete-state")
+        if comp_txt == "未完了":
+            print("未完了：\"{self.video_title.text}\"")
+            return 1
+
+        try:
+            next_url = self.driver.find_element(By.ID, "next-activity-link")
+            next_url.click()
+            return 0
+        except Exception as e:
+            print(e)
+            return 1
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {sys.argv[0]} <URL>")
+        print("URLs should be enclosed in single or double quotation marks.")
+    else:
+        viewer = BingeViewer(sys.argv[1])
+        viewer.start_viewing()
